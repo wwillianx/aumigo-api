201fb1ece1bd1a29e7b2808b070ab461a2d8c7ce
Willian Scheuermann <williamscheuermann10@gmail.com>
2024-11-20 18:18:39 -0300

    ajustes no app.e2e

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,22 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { UserService } from './../src/modules/user/user.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
+    const mockUserService = {
+      findAll: jest.fn().mockResolvedValue([]),  // Mock dos métodos do UserService
+    };
+
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(UserService)  // Sobrescrever o UserService com o mock
+      .useValue(mockUserService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
