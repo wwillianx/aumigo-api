25dc349e4c6fc055ff5bc4d18d2406858e2f450a
Willian Scheuermann <williamscheuermann10@gmail.com>
2024-11-20 18:40:09 -0300

    Revert "ajustes no app.e2e"
    This reverts commit 201fb1ece1bd1a29e7b2808b070ab461a2d8c7ce.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,22 +2,14 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { UserService } from './../src/modules/user/user.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
-    const mockUserService = {
-      findAll: jest.fn().mockResolvedValue([]),  // Mock dos métodos do UserService
-    };
-
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    })
-      .overrideProvider(UserService)  // Sobrescrever o UserService com o mock
-      .useValue(mockUserService)
-      .compile();
+    }).compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
